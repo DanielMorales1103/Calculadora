53e93c6b0a84382b74497b75ee81f8ddf1caddb4
DanielMorales1103 <91585288+DanielMorales1103@users.noreply.github.com>
2023-05-30 13:37:18 -0600

    Ajuste de tamaño
    Ajuste de tamaño de calculadora

--- src/CalculatorButton/CalculatorButton.js
+++ src/CalculatorButton/CalculatorButton.js
@@ -1,11 +1,11 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react'
 import './CalculatorButton.css'
 
 // eslint-disable-next-line react/prop-types
 export default function CalculatorButton({ label, onClick }) {
   return (
-    // eslint-disable-next-line max-len
-    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
     <div className="calculator-button" onClick={onClick}>
       {label}
     </div>
